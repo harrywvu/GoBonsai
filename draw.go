07678c4a08435f337f4bfeb427ed45d1dce06b3d
Harrywvu <johnny.vu2004@gmail.com>
2026-07-23 01:52:15 +0800

    add: draw base of bonsai vase (1/3)

--- draw.go
+++ draw.go
@@ -5,7 +5,27 @@ import "fmt"
 const (
 	WIDTH = 100
 	HEIGHT = 30
+	H_CENTER int = WIDTH / 2 
+	V_CEMTER int = HEIGHT / 2
+
+	BASE_FLAT_LENGTH int = 25
 )
+
+func DrawBase(grid [][]rune){
+	//
+	//	\                             /
+	//	 \							 /
+//		  \_________________________/
+//			--					 --
+	
+
+	start := H_CENTER - BASE_FLAT_LENGTH/2
+	end := H_CENTER + BASE_FLAT_LENGTH/2
+
+	for x := start; x <= end; x++ {
+		grid[27][x] = '_'
+	}
+}
 
 func DrawGrid(){
 
@@ -14,13 +34,15 @@ func DrawGrid(){
 	for y := range grid {
 		grid[y] = make([]rune, WIDTH)
 		for x := range grid[y] {
-			grid[y][x] = '.' // empty cell
+			grid[y][x] = ' ' // empty cell
 		}
 	}
 
 	// Place some characters.
-	grid[0][0] = '@'
-	grid[0][1] = '@'
+	// grid[0][0] = '@'
+	// grid[0][1] = '@'
+
+	DrawBase(grid)
 
 	// Draw the whole grid.
 	for _, row := range grid {

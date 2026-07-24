61dcde66efbca7029897ed6f62529d6d6fa877fc
Harrywvu <johnny.vu2004@gmail.com>
2026-07-24 23:00:46 +0800

    refactor: wrap grid in Window struct, make DrawBase relative to center

--- draw.go
+++ draw.go
@@ -5,81 +5,66 @@ import (
 	"math/rand/v2"	
 )
 
+type Window struct{
+	grid [][]rune
+	Width int
+	Height int
+}
+
+func NewWindow(w, h int) *Window{
+	grid := make([][]rune, h)
+	for y := range grid {
+		grid[y] = make([]rune, w)
+		for x:= range grid[y] {
+			grid[y][x] = ' '
+		}
+	}
+	return &Window{grid: grid, Width: w, Height: h}
+}
+
 const (
-	WIDTH = 100
-	HEIGHT = 30
-	H_CENTER int = WIDTH / 2 
-	V_CEMTER int = HEIGHT / 2
-
 	BASE_FLAT_LENGTH int = 25
-
-	Reset  = "\033[0m"
-	Green  = "\033[32m"
-	White  = "\033[37m"
-
-	
 )
 
-func DrawBase(grid [][]rune){
-	//
-	//	\_.^._~~..___..^..~~...***-_--/
-	//	 \							 /
-//		  \_________________________/
-//			--					 --
+func DrawBase(w *Window, centerCol int){
+	half := BASE_FLAT_LENGTH / 2
+	start := centerCol - half
+	end := centerCol + half
 
-	start := H_CENTER - BASE_FLAT_LENGTH/2
-	end := H_CENTER + BASE_FLAT_LENGTH/2
+	groundRow := w.Height - 3
 
 	for x := start; x <= end; x++ {
-		grid[27][x] = '_'
+		w.SetChar(groundRow, x, '_')
 	}
 
-	grid[28][39] = '-'
-	grid[28][40] = '-'
-	grid[28][61] = '-'
-	grid[28][60] = '-'
-
+	w.SetChar(groundRow+1, centerCol-half+1, '-')
+	w.SetChar(groundRow+1, centerCol-half+2, '-')
+	w.SetChar(groundRow+1, centerCol+half-1, '-')
+	w.SetChar(groundRow+1, centerCol+half-2, '-')
 	
-	i := 1
-	for y:= 27; y >= 25; y--{
-		for x:= start - i; x <= end + i; x++{
-			if x == start - i {
-				grid[y][x] = '\\'
-			}
-			if x == end + i {
-				grid[y][x] = '/'
-			}
-		}
-		i += 1
-	}
+	w.SetChar(groundRow, centerCol-half-1, '\\')
+	w.SetChar(groundRow, centerCol+half+1, '/')
+	w.SetChar(groundRow-1, centerCol-half-2, '\\')
+	w.SetChar(groundRow-1, centerCol+half+2, '/')
+	w.SetChar(groundRow-2, centerCol-half-3, '\\')
+	w.SetChar(groundRow-2, centerCol+half+3, '/')
 
 	chars := []rune{'.', ',', '~', '-', '_', '*', '^'}
 
 	for x := start - 2; x <= end + 2; x++{
-		grid[25][x] = chars[rand.IntN(len(chars))]
+		w.SetChar(groundRow-2, x, chars[rand.IntN(len(chars))])
 	}
 
 }
 
-func DrawGrid(){
-
-	// Create the buffer.
-	grid := make([][]rune, HEIGHT)
-	for y := range grid {
-		grid[y] = make([]rune, WIDTH)
-		for x := range grid[y] {
-			grid[y][x] = ' ' // empty cell
-		}
+func (w *Window) SetChar(row, col int, ch rune){
+	if row >= 0 && row < w.Height && col >= 0 && col < w.Width{
+		w.grid[row][col] = ch
 	}
+}
 
-	// Place some characters.
-	// grid[0][0] = '@'
-	// grid[0][1] = '@'
-
-	DrawBase(grid)
-
-	// Draw the whole grid.
-	for _, row := range grid {
+func (w *Window) Render(){
+	for _, row := range w.grid {
 		fmt.Println(string(row))
 	}
 }
--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
 func main() {
-	DrawGrid()
+	w := NewWindow(80, 25)
+    DrawBase(w, 40) // 40 = width/2, never hardcode once you add sin/cos
+    w.Render()
 }

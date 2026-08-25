1eb1bd75c37d5bc034025faed70d2d07741e2224
harrywvu <johnny.vu2004@gmail.com>
2026-08-25 09:51:07 +0800

    feat: rewrite main.go with new API (minimal stub)

--- main.go
+++ main.go
@@ -1,16 +1,33 @@
 package main
 
+import (
+	"os"
+)
+
 func main() {
-    w := NewWindow(80, 25)
-    DrawBase(w, 40)
+	args := os.Args[1:]
+	options := getOptions(args)
 
-    startX, startY := 40.0, 8.0
-    endX, endY := getEnd(startX, startY, 8, 0)
+	width, height := defaultWindowSize()
+	width = min(width, options.WindowWidth)
+	height = min(height, options.WindowHeight)
 
-    for y := startY; y <= endY; y++ {
-        w.SetCharPlane(startX, y, '|')
-    }
+	w := NewWindow(options.WindowWidth, options.WindowHeight, options)
 
-    w.Render()
+	// Draw a simple ground base
+	groundRow := w.height - 3
+	start := 40 - 25
+	end := 40 + 25
 
+	for x := start; x <= end; x++ {
+		w.SetCharPlane(float64(groundRow), float64(x), '_', fixedColour(200, 150, 0))
+	}
+
+	// Draw a simple trunk column
+	for y := float64(groundRow + 1); y <= float64(groundRow+3); y++ {
+		w.SetCharPlane(float64(40), y, '|', fixedColour(200, 200, 200))
+	}
+
+	w.Draw()
+	w.ResetCursor()
 }
